76efd2fbcf1f9610f1f13f4e48eebf5dc81520f8
Bruno <barbosa.bruno@me.com>
2019-02-25 20:40:09 -0300

    Setup new iOS project

---
+++ Virtual Tourist/Virtual Tourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Virtual Tourist
+//
+//  Created by Bruno Barbosa on 25/02/19.
+//  Copyright © 2019 Bruno Barbosa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
